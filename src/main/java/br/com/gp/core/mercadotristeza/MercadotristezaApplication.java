d1200e0db6afa97181455c5ba405762d6c87d05c
Pierre <pierrecosta2505@gmail.com>
2022-07-27 23:53:40 -0300

    segundo commit

---
+++ src/main/java/br/com/gp/core/mercadotristeza/MercadotristezaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gp.core.mercadotristeza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadotristezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadotristezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gp/core/mercadotristeza/MercadotristezaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gp.core.mercadotristeza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadotristezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
